d7a7a8c849f8b33e78eef0d8f85165967d83d376
turchynovychnazarii <Maykeros@gmail.com>
2022-10-14 11:29:58 +0300

    # This is a combination of 6 commits. # This is the 1st commit message:
    firstcommit
    
    # This is the commit message #2:
    
    secondcommit
    
    # This is the commit message #3:
    
    thirdcommit
    
    # This is the commit message #4:
    
    rebasedbranch
    
    # This is the commit message #5:
    
    # This is a combination of 4 commits.
    # This is the 1st commit message:
    
    4commit
    
    # This is the commit message #2:
    
    5com
    
    # This is the commit message #3:
    
    rebasssssssseddddddd
    
    # This is the commit message #4:
    
    noviycom
    
    # This is the commit message #6:
    
    newwwcom

--- Domain/Class1.cs
+++ Domain/Class1.cs
@@ -2,9 +2,27 @@
 
 public class Class1
 {
-    public int Name1 { get; set; }
+    public int Name141 { get; set; }
     
-    public int Name2 { get; set; }
+    public int Name25 { get; set; }
     
     public int Name3 { get; set; }
+    
+    public int novayebanahuynya { get; set; }
+    
+    public int Name4 { get; set; }
+    
+
+    public int Name5 { get; set; }
+    
+    public int Name6 { get; set; }
+
+    public int Name7 { get; set; }
+    
+    public int sadsadsa { get; set; }
+
+    public int Name7 { get; set; }
+
+    public int Newpropinsecondbranch { get; set; }
+
 }
